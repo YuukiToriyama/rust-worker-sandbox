e004a8f5ab28b51fd3c6a6f9966c721ab2bf8100
Yuuki Toriyama <github@toriyama.dev>
2023-06-26 00:57:45 +0900

    Helloworldを返すようにした

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use worker::{event, Context, Env, Request, Response, Result};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[event(fetch)]
+async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    Response::ok("Hello, World!")
 }
